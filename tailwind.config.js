c146debc194c254e5698e3f4c5a6deec22f3114b
factualmayur <factualmayur@gmail.com>
2024-12-05 12:45:05 +0530

    up

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./*.{html,js}", // Adjust according to your file structure
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
